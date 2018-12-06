e782f846b14b93502cdeb753dab710ee9b085681
royshahaf <royshahaf@gmail.com>
2018-12-06 13:52:22 +0200

    leader election basic tests

--- src/main/java/basic/Basic.java
+++ src/main/java/basic/Basic.java
@@ -1,9 +1,61 @@
 package basic;
 
-public class Basic {
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
-	public boolean isBasic() {
-		return true;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.leader.LeaderSelector;
+import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+public class Basic extends LeaderSelectorListenerAdapter implements Closeable {
+	private static final String PATH = "/examples/leader";
+	private final LeaderSelector leaderSelector;
+	private final CuratorFramework client;
+	private String name;
+	private final AtomicBoolean stopped = new AtomicBoolean(false);
+	private final AtomicInteger leadCount = new AtomicInteger(0);
+
+	public int getLeadCount() {
+		return leadCount.get();
+	}
+
+	public Basic(String connectionString, String name) {
+		client = CuratorFrameworkFactory.newClient(connectionString, new ExponentialBackoffRetry(1000, 3));
+		this.name = name;
+		leaderSelector = new LeaderSelector(client, PATH, this);
+		leaderSelector.autoRequeue();
+	}
+
+	public void start() throws IOException {
+		client.start();
+		leaderSelector.start();
+	}
+	
+	public void stop() {
+		stopped.set(true);
+	}
+
+	@Override
+	public void takeLeadership(CuratorFramework client) throws Exception {
+		leadCount.incrementAndGet();
+		while (!getStopped()) {
+			System.out.println("master " + name);
+			Thread.sleep(1000);
+		}
+	}
+
+	@Override
+	public void close() {
+		stop();
+		leaderSelector.close();
+	}
+
+	public boolean getStopped() {
+		return stopped.get();
 	}
 
 }
--- src/test/java/basic/BasicTest.java
+++ src/test/java/basic/BasicTest.java
@@ -1,6 +1,15 @@
 package basic;
-import static org.junit.Assert.*;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.curator.test.TestingServer;
+import org.awaitility.Awaitility;
 import org.junit.Test;
 
 import basic.Basic;
@@ -8,9 +17,38 @@ import basic.Basic;
 public class BasicTest {
 
 	@Test
-	public void test() {
-		Basic basic = new Basic();
-		assertTrue(basic.isBasic());
+	public void testStart() throws Exception {
+		try (TestingServer server = new TestingServer();
+				Basic basic = new Basic(server.getConnectString(), "stupid1")) {
+			basic.start();
+			Awaitility.await().untilAsserted(() -> assertEquals(1, basic.getLeadCount()));
+		}
+	}
+
+	@Test
+	public void testStop() throws Exception {
+		try (TestingServer server = new TestingServer();
+				Basic basic = new Basic(server.getConnectString(), "stupid1")) {
+			basic.start();
+			Awaitility.await().untilAsserted(() -> assertEquals(1, basic.getLeadCount()));
+			Awaitility.await().untilAsserted(() -> assertFalse(basic.getStopped()));
+			basic.stop();
+			Awaitility.await().untilAsserted(() -> assertTrue(basic.getStopped()));
+		}
+	}
+
+	@Test
+	public void testClose() throws Exception {
+		Basic sillyBasic;
+		try (TestingServer server = new TestingServer();
+				Basic basic = new Basic(server.getConnectString(), "stupid1")) {
+			sillyBasic = basic;
+			basic.start();
+			Awaitility.await().untilAsserted(() -> assertEquals(1, basic.getLeadCount()));
+			Awaitility.await().untilAsserted(() -> assertFalse(basic.getStopped()));
+		}
+		assertNotNull(sillyBasic);
+		Awaitility.await().untilAsserted(() -> assertTrue(sillyBasic.getStopped()));
 	}
 
 }
