182072141d4b64c973f9cca532fdc98641d03f8c
royshahaf <royshahaf@gmail.com>
2018-12-06 20:01:33 +0200

    don't use consul in tests (they run on some server without access)

--- src/test/java/basic/BasicTest.java
+++ src/test/java/basic/BasicTest.java
@@ -5,20 +5,23 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.curator.test.TestingServer;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.awaitility.Awaitility;
 import org.awaitility.Duration;
 import org.cfg4j.provider.ConfigurationProvider;
 import org.cfg4j.provider.ConfigurationProviderBuilder;
 import org.cfg4j.source.ConfigurationSource;
-import org.cfg4j.source.consul.ConsulConfigurationSourceBuilder;
+import org.cfg4j.source.context.environment.ImmutableEnvironment;
+import org.cfg4j.source.context.filesprovider.ConfigFilesProvider;
+import org.cfg4j.source.files.FilesConfigurationSource;
 import org.cfg4j.source.reload.strategy.PeriodicalReloadStrategy;
 import org.junit.Test;
 
@@ -31,10 +34,15 @@ public class BasicTest {
 	}
 
 	private BasicConfiguration initConfiguration() {
-		ConfigurationSource source = new ConsulConfigurationSourceBuilder().withHost("192.168.99.100").withPort(8500)
-				.build();
+		/*
+		 * ConfigurationSource source = new
+		 * ConsulConfigurationSourceBuilder().withHost(ip).withPort(port).build();
+		 */
+		ConfigFilesProvider configFilesProvider = () -> Arrays.asList(Paths.get("application.yaml"));
+		ConfigurationSource source = new FilesConfigurationSource(configFilesProvider);
+		ImmutableEnvironment environment = new ImmutableEnvironment(".");
 		ConfigurationProvider provider = new ConfigurationProviderBuilder().withConfigurationSource(source)
-				.withReloadStrategy(new PeriodicalReloadStrategy(5, TimeUnit.SECONDS)).build();
+				.withReloadStrategy(new PeriodicalReloadStrategy(5, TimeUnit.SECONDS)).withEnvironment(environment).build();
 		return provider.bind("reksio", BasicConfiguration.class);
 	}
 
@@ -87,7 +95,7 @@ public class BasicTest {
 			Awaitility.await().untilAsserted(() -> assertEquals(1, basic.getLeadCount()));
 		}
 	}
-	
+
 	@Test
 	public void testBasicConfigurationNameChange() throws IOException, Exception {
 		setValue("a");
@@ -97,11 +105,21 @@ public class BasicTest {
 	}
 
 	private void setValue(String value) throws UnsupportedEncodingException, IOException, ClientProtocolException {
-		try (CloseableHttpClient client = HttpClients.createDefault()) {
-			HttpPut request = new HttpPut("http://192.168.99.100:8500/v1/kv/reksio/name");
-			request.setEntity(new StringEntity(value));
-			client.execute(request);
+		/*
+		 * try (CloseableHttpClient client = HttpClients.createDefault()) { HttpPut
+		 * request = new HttpPut("http://ip:port/v1/kv/reksio/name");
+		 * request.setEntity(new StringEntity(value)); client.execute(request); }
+		 */
+		List<String> before = Files.readAllLines(Paths.get("application.yaml"));
+		List<String> after = new ArrayList<>();
+		for (String line : before) {
+			if (line.contains("name")) {
+				after.add("  name: " + value);
+			} else {
+				after.add(line);
+			}
 		}
+		Files.write(Paths.get("application.yaml"), after);
 	}
 
 }
